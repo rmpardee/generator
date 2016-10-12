c8a8e600385c0fc2a120758e7decffb24217f7a7
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-10-12 13:53:38 -0700

    Functioning, formatting poor.

--- generate.js
+++ generate.js
@@ -14,42 +14,42 @@ var company = {
 };
 
 
-var letter = `\n
-1st paragraph:
-1st sentence: this is the KEY
-Don’t tell them something they already know
-Need a HOOK: connect yourself to the company product; how something you do relates to their product; can be technical or personal; mirror the job post for guidance; connect the skill you have to something the company values (use what they give you)
-2nd two sentences
-Expand on what you bring to the company \n
-\n
-I can’t think of a better work scenario than the combination of being a part of a ${company.teamAspect} team like ${company.name}, but with the impact to work on a product with ${company.productAspect}. I appreciate ${company.name}’s devotion to ${company.teamMO1}; I believe in ${company.teamValue} to reach ${company.productGoal}. I thrive when I am constantly presented with new challenges, and it is clear to me this role at ${company.name} would provide this type of opportunity. \n
-\n
-2nd paragraph:
-Focus on hard skills, key languages, relevant work history
-Focus on what’s really impressive
-You should be able to verbalize what’s important & why
-Doesn’t have to be long; could be two sentences!
-1 sentence abt something unique/targeted to the comp
-2nd sentence: use the company name \n
-\n
-I am a full-stack engineer with strengths in JavaScript and modern frameworks such as ${company.frameworks}. I also am experienced with ${company.otherTechnology}. I am highly detail-oriented and appreciate ${company.teamMO2} approaches to software engineering like ${company.name} uses. As a former Product Manager, I understand well how ${company.workflow} workflows affect the overall product, and would be excited to be part of a team that values these methods as well, especially for such a ${company.productPride} application. \n
-\n
-Last paragraph: 
-Short description of one specific recent project 
-something that’s relevant to the company
-What part of your project is relevant to the company
-Don’t have any relevant projects? 
-Do the most impressive. 
-LINK to the project! They should be able to click on something that makes you look good!
-Closing:
-don’t equivocate; convey that you’ve already been inducted into their cult; assume they want to interview you 
-Close with language they use on their website. \n
-\n
-${company.gourmand} My combination of extensive knowledge of JavaScript and of software development ${company.workflow} workflows would make me able to jump right into the code-base at ${company.name} on day one.`;
-
+function makeLetter() {
+  return `
+  <i>1st paragraph:
+  1st sentence: this is the KEY
+  Don’t tell them something they already know
+  Need a HOOK: connect yourself to the company product; how something you do relates to their product; can be technical or personal; mirror the job post for guidance; connect the skill you have to something the company values (use what they give you)
+  2nd two sentences
+  Expand on what you bring to the company.</i></br>
+  </br>
+  I can’t think of a better work scenario than the combination of being a part of a ${company.teamAspect} team like ${company.name}, but with the impact to work on a product with ${company.productAspect}. I appreciate ${company.name}’s devotion to ${company.teamMO1}; I believe in ${company.teamValue} to reach ${company.productGoal}. I thrive when I am constantly presented with new challenges, and it is clear to me this role at ${company.name} would provide this type of opportunity. </br>
+  </br>
+  <i>2nd paragraph:
+  Focus on hard skills, key languages, relevant work history
+  Focus on what’s really impressive
+  You should be able to verbalize what’s important & why
+  Doesn’t have to be long; could be two sentences!
+  1 sentence abt something unique/targeted to the comp
+  2nd sentence: use the company name.</i></br>
+  </br>
+  I am a full-stack engineer with strengths in JavaScript and modern frameworks such as ${company.frameworks}. I also am experienced with ${company.otherTechnology}. I am highly detail-oriented and appreciate ${company.teamMO2} approaches to software engineering like ${company.name} uses. As a former Product Manager, I understand well how ${company.workflow} workflows affect the overall product, and would be excited to be part of a team that values these methods as well, especially for such a ${company.productPride} application. </br>
+  </br>
+  <i>Last paragraph: 
+  Short description of one specific recent project 
+  something that’s relevant to the company
+  What part of your project is relevant to the company
+  Don’t have any relevant projects? 
+  Do the most impressive. 
+  LINK to the project! They should be able to click on something that makes you look good!
+  Closing:
+  don’t equivocate; convey that you’ve already been inducted into their cult; assume they want to interview you 
+  Close with language they use on their website.</i></br>
+  </br>
+  ${company.gourmand} My combination of extensive knowledge of JavaScript and of software development ${company.workflow} workflows would make me able to jump right into the code-base at ${company.name} on day one.`;
+};
 
 function setValues(inputs) {
-    console.log("company: ", company);
   for (var i = 0; i < inputs.length; i++) {
     inputs[i].value = company[inputs[i].name];
   }
@@ -57,18 +57,17 @@ function setValues(inputs) {
 
 $(function() {
   setValues($('form')[0]);
-  $('#letter').text(letter);
+  $('#letter').html(makeLetter());
 });
 
-function generateLetter(inputs) {
+function regenerateLetter(inputs) {
   for (var i = 0; i < inputs.length; i++) {
-    console.log("company: ", company);
     company[inputs[i].name] = inputs[i].value;
   }
-  $('#letter').text(letter);
+  $('#letter').html(makeLetter());
 };
 
-$('form').on('submit', function() {
-  // preventDefault(e);
-  generateLetter($('form'));
+$('form').on('submit', function(e) {
+  e.preventDefault();
+  regenerateLetter($('form')[0]);
 });                                             
